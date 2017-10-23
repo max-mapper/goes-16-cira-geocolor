f0df0a7b35bf1d5eaade0d2d9aa4ef4977a9f107
Max Ogden <max@maxogden.com>
2017-10-23 18:43:27 +0200

    merge build script

--- movie.sh
+++
@@ -1,4 +0,0 @@
-rm -rf movietmp
-mkdir movietmp
-ls overlay | tail -n -96 | xargs -I {} cp overlay/{} movietmp
-ffmpeg -y -r 5 -pattern_type glob -i 'movietmp/*.png' -vf scale=2034:-1 -vcodec libx264 -crf 25 24hrs.mp4
--- render.sh
+++ render.sh
@@ -3,3 +3,7 @@ for f in renders/*.png; do
   filename=$(basename $f .JPG)
   convert $f -fill white -pointsize 30 -gravity NorthWest -draw "text 10,10 '$(node date.js $filename)'" -pointsize 20 -draw "text 10,50 'CIRA GeoColor, NASA GOES-16 Satellite'" overlay/$filename
 done
+rm -rf movietmp
+mkdir movietmp
+ls overlay | tail -n -96 | xargs -I {} cp overlay/{} movietmp
+ffmpeg -y -r 5 -pattern_type glob -i 'movietmp/*.png' -vf scale=2034:-1 -vcodec libx264 -crf 25 24hrs.mp4
