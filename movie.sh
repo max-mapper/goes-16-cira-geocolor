87aa1fac439de63002d43928f24e15bc6a280e9b
Max Ogden <max@maxogden.com>
2017-10-23 18:39:38 +0200

    better movie build

--- movie.sh
+++ movie.sh
@@ -1,1 +1,4 @@
-ffmpeg -r 10 -pattern_type glob -i 'renders/*.png' -vf scale=2034:-1 -vcodec libx264 -crf 25 output.mp4
+rm -rf movietmp
+mkdir movietmp
+ls overlay | tail -n -96 | xargs -I {} cp overlay/{} movietmp
+ffmpeg -y -r 5 -pattern_type glob -i 'movietmp/*.png' -vf scale=2034:-1 -vcodec libx264 -crf 25 24hrs.mp4
