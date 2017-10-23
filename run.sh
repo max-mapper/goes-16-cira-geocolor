7e93b82739a30a1994a04cafd511ec3d86cbe238
Max Ogden <max@maxogden.com>
2017-10-23 18:55:36 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+node index.js # downloads latest
 find . -name "*.png" -size -1k -delete # if last ran exited w/ half written files
 ls images | xargs -I {} sh -c "if [ ! -f renders/{}.png ]; then montage images/{}/*.png -tile 3x3 -geometry +0+0 -background none renders/{}.png; fi"
 for f in renders/*.png; do
@@ -8,5 +9,5 @@ for f in renders/*.png; do
 done
 rm -rf movietmp
 mkdir movietmp
-ls overlay | tail -n -96 | xargs -I {} cp overlay/{} movietmp
+ls overlay | tail -n -96 | xargs -I {} cp overlay/{} movietmp # only build last 96 (last 24 hours worth)
 ffmpeg -y -r 5 -pattern_type glob -i 'movietmp/*.png' -vf scale=2034:-1 -vcodec libx264 -crf 25 24hrs.mp4
