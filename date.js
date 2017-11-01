813be0af1106ff13d0e1bf6c798b23878b93a227
Mac Cowell <mac@has100ideas.com>
2017-10-31 21:15:18 -0700

    properly shift image tz from utc->pacific

--- date.js
+++ date.js
@@ -2,4 +2,7 @@ var moment = require('moment-timezone')
 var date = process.argv[2]
 date = `${date.slice(0, 8)}T${date.slice(8, 14)}`
 console.error(date)
-console.log(moment(date).tz('America/Los_Angeles').format("dddd, MMMM Do YYYY, h:mma z"))
+// moment-timezone assumes data param is given in LOCAL system timezone; so tell it its UTC
+// console.log(moment(date).tz('America/Los_Angeles').format("dddd, MMMM Do YYYY, h:mma z"))
+console.log(moment.tz(date, 'utc').tz('America/Los_Angeles').format("dddd, MMMM Do YYYY, h:mma z"))
+
